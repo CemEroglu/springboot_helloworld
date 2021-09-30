2cccf7f0ec711929314c858f20200c3b982efe0e
TURKCELL\tcceroglu <cem.eroglu@turkcell.com.tr>
2021-09-30 20:20:22 +0300

    Hello World Completed!

---
+++ src/main/java/com/cemkodla/cmhelloworld/CmHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.cemkodla.cmhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmHelloWorldApplication.class, args);
+	}
+
+}
